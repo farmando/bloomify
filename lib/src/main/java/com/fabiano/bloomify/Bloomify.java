d2ff8a4974a4a86f650ebac55d8bf38f97ad7b50
fabiano armando <fabianoarmando.fa@gmail.com>
2023-06-25 16:30:26 +0300

    implemented bloom filter and make tests green

--- lib/src/main/java/com/fabiano/bloomify/Bloomify.java
+++ lib/src/main/java/com/fabiano/bloomify/Bloomify.java
@@ -1,23 +1,93 @@
 package com.fabiano.bloomify;
 
-import jdk.jshell.spi.ExecutionControl;
+import static java.lang.Math.ceil;
+import static java.lang.Math.log;
+import static java.lang.Math.max;
+import static java.lang.Math.pow;
+import static java.lang.Math.round;
 
+import com.fabiano.bloomify.strategy.LocalStorage;
+import com.fabiano.bloomify.strategy.StorageStrategy;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class Bloomify {
-    public Bloomify(int expectedInsertions, double falsePositiveRate) {
+  private final StorageStrategy storageStrategy;
+  private final int setSize;
+  private final List<Function<String, Integer>> hashFunctions;
 
+  public Bloomify(int expectedInsertions, double falsePositiveRate) {
+    this(expectedInsertions, falsePositiveRate, new LocalStorage(expectedInsertions));
+  }
+
+  public Bloomify(int expectedInsertions, double falsePositiveRate, StorageStrategy strategy) {
+    if (expectedInsertions <= 0 || falsePositiveRate <= 0 || falsePositiveRate >= 1) {
+      throw new IllegalArgumentException("Invalid parameters");
     }
 
-    public boolean contains(String value) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("not implemented");
-    }
+    this.setSize = calcSetSizeFor(expectedInsertions, falsePositiveRate);
+    int numberOfHashFunctions = calcNumberOfHashFunctionsFor(expectedInsertions, setSize);
+    this.storageStrategy = strategy;
+    this.hashFunctions = generateHashFunctions(numberOfHashFunctions);
+  }
 
-    public void add(String element) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("not implemented");
-    }
+  public boolean contains(String value) {
+    return hashFunctions.stream().allMatch(function -> storageStrategy.contains(function.apply(value)));
+  }
 
-    public void addAll(List<String> elements) throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("not implemented");
-    }
+  public void add(String element) {
+    hashFunctions.forEach(hash -> storageStrategy.add(hash.apply(element.trim())));
+  }
+
+  public void addAll(List<String> values) {
+    int threadPoolSize = calcThreadPoolSize();
+    int chunkSize = calcChunkSize(values, threadPoolSize);
+    ExecutorService executor = Executors.newFixedThreadPool(threadPoolSize);
+
+    IntStream.range(0, threadPoolSize).forEach(t -> executor.execute(() -> {
+      int fromIndex = t * chunkSize;
+      int toIndex = Math.min(fromIndex + chunkSize, values.size());
+      if (fromIndex < toIndex) {
+        IntStream.range(fromIndex, toIndex).forEach(i -> add(values.get(i)));
+      }
+    }));
+    executor.shutdown();
+  }
+
+  private List<Function<String, Integer>> generateHashFunctions(int numberOfHashFunctions) {
+    List<Function<String, Integer>> functions = new ArrayList<>();
+    IntStream.range(0, numberOfHashFunctions).forEach(seed -> {
+      Function<String, Integer> hashFunction = element -> {
+        AtomicInteger hashFactor = new AtomicInteger(0);
+        element.chars().forEach(e -> hashFactor.set((hashFactor.incrementAndGet() * seed) + e));
+        return Math.abs(hashFactor.get() % setSize);
+      };
+      functions.add(hashFunction);
+    });
+    return functions;
+  }
+
+  private int calcNumberOfHashFunctionsFor(int expectedInsertions, int size) {
+    return max(1, (int) round(((double) size / expectedInsertions) * log(2)));
+  }
+
+  private int calcSetSizeFor(int expectedInsertions, double falsePositiveRate) {
+    return (int) ceil((expectedInsertions * log(falsePositiveRate)) / log(1.0 / (pow(2.0, log(2.0)))));
+  }
+
+  private int calcThreadPoolSize() {
+    //TODO - what is the best way to calculate the number of thread pool size
+    int numOfCores = Runtime.getRuntime().availableProcessors();
+    return numOfCores / 2 + 1;
+  }
+
+  private int calcChunkSize(List<String> values, int threadPoolSize) {
+    int chunkSize = (values.size() / threadPoolSize);
+    return chunkSize > 0 ? chunkSize : 1;
+  }
 }
--- lib/src/test/java/com/fabiano/bloomify/BloomifyTest.java
+++ lib/src/test/java/com/fabiano/bloomify/BloomifyTest.java
@@ -1,61 +1,61 @@
 package com.fabiano.bloomify;
 
-import jdk.jshell.spi.ExecutionControl;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.*;
-
 class BloomifyTest {
 
-    private Bloomify bloomify;
+  private Bloomify bloomify;
 
-    @BeforeEach
-    public void setUp() {
-        bloomify = new Bloomify(10000000, 0.01);
-    }
+  @BeforeEach
+  public void setUp() {
+    bloomify = new Bloomify(10000000, 0.01);
+  }
 
-    @Test
-    void bloomSense_canAddStringToSet() throws ExecutionControl.NotImplementedException {
-        var registeredEmail = "registered@email.com";
-        var registeredEmail2 = "registered2@email.com";
-        var registeredEmail3 = "registered3@email.com";
-        var registeredEmail4 = "registered4@email.com";
+  @Test
+  void bloomSense_canAddStringToSet() {
+    var registeredEmail = "registered@email.com";
+    var registeredEmail2 = "registered2@email.com";
+    var registeredEmail3 = "registered3@email.com";
 
-        bloomify.add(registeredEmail);
-        bloomify.add(registeredEmail2);
-        bloomify.add(registeredEmail3);
+    bloomify.add(registeredEmail);
+    bloomify.add(registeredEmail2);
+    bloomify.add(registeredEmail3);
 
-        assertTrue(bloomify.contains(registeredEmail));
-        assertTrue(bloomify.contains(registeredEmail2));
-        assertTrue(bloomify.contains(registeredEmail3));
-        assertFalse(bloomify.contains(registeredEmail4));
-    }
+    assertTrue(bloomify.contains(registeredEmail));
+    assertTrue(bloomify.contains(registeredEmail2));
+    assertTrue(bloomify.contains(registeredEmail3));
 
-    @Test
-    void bloomSense_canAddListOfStringToSet() throws ExecutionControl.NotImplementedException {
-        var registeredEmail = "registered@email.com";
-        var anotherRegisteredEmail = "another@email.com";
-        List<String> registeredList = List.of(registeredEmail, anotherRegisteredEmail);
+    var registeredEmail4 = "registered4@email.com";
+    assertFalse(bloomify.contains(registeredEmail4));
+  }
 
-        bloomify.addAll(registeredList);
+  @Test
+  void bloomSense_canAddListOfStringToSet() {
+    var registeredEmail = "registered@email.com";
+    var anotherRegisteredEmail = "another@email.com";
+    List<String> registeredList = List.of(registeredEmail, anotherRegisteredEmail);
 
-        await().atMost(150, MILLISECONDS).untilAsserted(() -> {
-            assertTrue(bloomify.contains(registeredEmail));
-            assertTrue(bloomify.contains(anotherRegisteredEmail));
-        });
-    }
+    bloomify.addAll(registeredList);
 
-    @Test
-    void bloomSense_willThrowsExceptionWhenInvalidParameter() {
-        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class,
-                () -> new Bloomify(10000000, 0.00));
+    await().atMost(150, MILLISECONDS).untilAsserted(() -> {
+      assertTrue(bloomify.contains(registeredEmail));
+      assertTrue(bloomify.contains(anotherRegisteredEmail));
+    });
+  }
 
-        Assertions.assertEquals("Invalid parameters", thrown.getMessage());
-    }
+  @Test
+  void bloomSense_willThrowsExceptionWhenInvalidParameter() {
+    IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class,
+            () -> new Bloomify(10000000, 0.00));
+
+    Assertions.assertEquals("Invalid parameters", thrown.getMessage());
+  }
 }
